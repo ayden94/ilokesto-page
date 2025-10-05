2119784abb96fbf70576b758170f4a89703012b5
Jinho Ayden Jeong <144667387+ayden94@users.noreply.github.com>
2025-10-05 13:39:32 +0900

    style: update Sicilian and Carokann color variables; adjust z-index in Header component for improved layering

--- src/ui/widgets/Header.tsx
+++ src/ui/widgets/Header.tsx
@@ -5,7 +5,7 @@ import { ThemeToggler } from '../features/ThemeToggler'
 
 export function Header({ githubLink, title }: { githubLink: string; title: string }) {
   return (
-    <header className="sticky top-0 z-auto custom-shadow">
+    <header className="sticky top-0 z-50 custom-shadow">
       <div className="flex items-center h-16 max-w-[1440px] mx-auto p-6">
         <h1 className="flex-grow text-2xl/6 font-medium">{title}</h1>
 
