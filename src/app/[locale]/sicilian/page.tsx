d4c58485e2c6ba5920950af4f3c5657484d32425
Jinho Ayden Jeong <144667387+ayden94@users.noreply.github.com>
2025-10-06 16:11:22 +0900

    feat: sicilian 페이지 및 다국어 지원 추가

--- src/app/[locale]/sicilian/page.tsx
+++ src/app/[locale]/sicilian/page.tsx
@@ -1,3 +1,48 @@
-export default function Page() {
-  return <></>
+import { codeFormatter } from '@/shared/utils/code'
+import { CodeBlock } from '@/ui/components/CodeBlock'
+import { Heading } from '@/ui/components/Heading'
+import List from '@/ui/components/List'
+import { Textline } from '@/ui/components/Text'
+import { getTranslations } from 'next-intl/server'
+import Link from 'next/link'
+
+export default async function Page() {
+  const t = await getTranslations('sicilian.introduce')
+  const codeFormatObject = codeFormatter()
+
+  return (
+    <>
+      <Heading.h1>{t('title')}</Heading.h1>
+
+      <Textline>{t.rich('description', codeFormatObject)}</Textline>
+
+      <Heading.h2>{t('whatsNew')}</Heading.h2>
+
+      <List.ul>
+        {(['0', '1', '2', '3', '4'] as const).map((num) => (
+          <List.item key={num}>
+            {t.rich(`list.${num}`, {
+              ...codeFormatObject,
+              a: (chunk) => (
+                <Link className="link" href="">
+                  {chunk}
+                </Link>
+              ),
+            })}
+          </List.item>
+        ))}
+      </List.ul>
+
+      <Heading.h2>{t('installation')}</Heading.h2>
+
+      <Textline>{t('installationBody')}</Textline>
+
+      <CodeBlock filename="install.sh" language="bash">
+        {`npm install sicilian
+pnpm add sicilian
+yarn add sicilian
+bun add sicilian`}
+      </CodeBlock>
+    </>
+  )
 }
--- src/shared/i18n/messages/en/index.ts
+++ src/shared/i18n/messages/en/index.ts
@@ -2,10 +2,12 @@ import caroKann from './caro-kann.json'
 import commonResolver from './common-resolver.json'
 import ilokesto from './ilokesto.json'
 import pathCodegen from './path-codegen.json'
+import sicilian from './sicilian.json'
 
 export default {
   ilokesto,
   caroKann,
   commonResolver,
+  sicilian,
   pathCodegen,
 }
